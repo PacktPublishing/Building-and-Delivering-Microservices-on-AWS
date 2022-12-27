01138cc30a0ceab1c0ebb51f8f123586ab9a79ff
Amar deep Singh <amardeep_singh@Amars-MBP.attlocal.net>
2022-12-26 21:35:40 -0600

    Added chapter 3

---
+++ chapter_3/aws-code-pipeline/src/main/java/com/packt/aws/books/pipeline/AwsCodePipelineApplication.java
@@ -0,0 +1,13 @@
+package com.packt.aws.books.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodePipelineApplication.class, args);
+	}
+
+}
---
+++ chapter_3/aws-code-pipeline/src/test/java/com/packt/aws/books/pipeline/awscodepipeline/AwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.aws.books.pipeline.awscodepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
