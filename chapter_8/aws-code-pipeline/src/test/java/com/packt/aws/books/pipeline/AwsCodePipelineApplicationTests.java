a0a1e76d45e19dcfa13385ac159170110fee74fe
Amar deep Singh <amardeep_singh@Amars-MBP.attlocal.net>
2023-01-29 19:19:58 -0600

    Added code for chapter 8

---
+++ chapter_8/aws-code-pipeline/src/test/java/com/packt/aws/books/pipeline/AwsCodePipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.aws.books.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCodePipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
